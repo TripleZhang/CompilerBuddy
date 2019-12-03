84420460f4903646ba13edd5266a775cee603c75
zhanghb97 <zhanghb97@126.com>
2019-12-04 00:51:46 +0800

    修改judge bug

--- tiger/judge.py
+++ tiger/judge.py
@@ -3,11 +3,13 @@ import os
 def judge(dir):
     # dir = "./tiger/code"
     # 在指定文件目录make代码
-    os.popen("sh make.sh " + dir)
+    os.system("sh make.sh " + dir)
     # 在指定文件目录执行可执行文件
     pipline = os.popen("sh runtiger.sh " + dir)
+    result = pipline.read()
+    print (result)
     # 判断是否结果是否正确
-    if pipline.read() == "1":
+    if result == "1":
         return 1
     else:
         return 0

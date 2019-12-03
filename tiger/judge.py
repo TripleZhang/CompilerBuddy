01a53f743940683fb28efa4a4278a32592939f3e
zhanghb97 <zhanghb97@126.com>
2019-12-03 11:16:29 +0800

    增加判断正误模块路径参数传递

--- tiger/judge.py
+++ tiger/judge.py
@@ -1,24 +1,13 @@
-# import os
-# from Compiler_teaching_platform.settings import USER_ENVIRONMENTS
-# def judge(user_dir,course_dir):
-#     dir = USER_ENVIRONMENTS+user_dir+course_dir
-#     print(dir)
-#     dir = '/Users/zach/PycharmProjects/CompilerBuddy/user_environments/1888888888/course1/code/make.sh'
-#     pipline = os.popen("sh " + dir)
-#
-#     return pipline.read()
-#
-# if __name__ == '__main__':
-#     user_dir = '18888888888/'
-#     course_dir = 'course1/code/make.sh'
-#     judge(user_dir,course_dir)
-
-
 import os
 
 def judge(dir):
-    pipline = os.popen("sh make.sh")
-    return pipline.read()
-
-if __name__ == '__main__':
-    print(judge(''))
+    dir = "./tiger/code"
+    # 在指定文件目录make代码
+    os.popen("sh make.sh " + dir)
+    # 在指定文件目录执行可执行文件
+    pipline = os.popen("sh runtiger.sh " + dir)
+    # 判断是否结果是否正确
+    if pipline.read() == "1":
+        return 1
+    else:
+        return 0

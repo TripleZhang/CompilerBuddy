731328dff4e2cb9ff661d9df39444134ecaeb7cc
zhanghb97 <zhanghb97@126.com>
2019-12-04 16:31:46 +0800

    更新judge模块输出

--- tiger/judge.py
+++ tiger/judge.py
@@ -3,16 +3,16 @@ import os
 def judge(dir):
     # dir = "./tiger/code"
     # 在指定文件目录make代码
-    os.system("sh make.sh " + dir)
+    makePipline = os.popen("sh make.sh " + dir)
+    makeResult = makePipline.read()
     # 在指定文件目录执行可执行文件
     pipline = os.popen("sh runtiger.sh " + dir)
     result = pipline.read()
-    print (result)
     # 判断是否结果是否正确
     if result == "1":
-        return 1
+        return "Pass"
     else:
-        return 0
+        return makeResult
 
 if __name__ == '__main__':
     print(judge('/Users/zhanghongbin/Desktop/CompilerBuddy/user_environments/1888888888/course1/code'))
